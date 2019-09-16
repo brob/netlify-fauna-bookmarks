5e2d22c810366c8eda743ad16fae47016be9d612
Bryan Robinson <bryanlrobinson@gmail.com>
2019-09-16 09:10:32 -0500

    Abstracts create functions to a new file

--- lambda/bookmarks.js
+++ lambda/bookmarks.js
@@ -1,57 +1,15 @@
-const rp = require('request-promise');
-const cheerio = require('cheerio');
-var faunadb = require('faunadb'),
-    q = faunadb.query;
-
-var adminClient = new faunadb.Client({
-    secret: process.env.FAUNADB_SERVER_SECRET
-});
-
-async function getDetails(url) {
-  const data = rp(url).then(function(htmlString) {
-    const $ = cheerio.load(htmlString);
-    const title = $('head > title').text();
-    const description = $('meta[name="description"]').attr('content');
-    return {
-      pageTitle: title,
-      description: description
-    };
-  });
-  return data
-}
-
-async function saveBookmark(details) {
-  const data = {
-    data: details
-  };
-  return adminClient.query(q.Create(q.Collection("links"), data))
-    .then((response) => {
-      console.log("success", response)
-      /* Success! return the response with statusCode 200 */
-      return {
-        statusCode: 200,
-        body: JSON.stringify(response)
-      }
-    }).catch((error) => {
-      console.log("error", error)
-      /* Error! return the error with statusCode 400 */
-      return  {
-        statusCode: 400,
-        body: JSON.stringify(error)
-      }
-    })
-  console.log(details);
-}
+import { getDetails, saveBookmark } from "./bookmarks/create";
 
 exports.handler = async function(event, context) {
   try {
     const url = event.queryStringParameters.url;
     const details = await getDetails(url);
     
-    const savedResponse = saveBookmark({url, ...details})
-    return savedResponse
+    const savedResponse = await saveBookmark({url, ...details});
+
+    return { statusCode: 200, body: savedResponse.body }
   } catch (err) {
-    return { statusCode: 500, body: "lambda failure code!" };
+    return { statusCode: 500, body: `Error: ${err}` };
   }
 
 };
--- lambda/bookmarks/create.js
+++ lambda/bookmarks/create.js
@@ -1,90 +1,46 @@
 const rp = require('request-promise');
 const cheerio = require('cheerio');
-
-const faunadb = require('faunadb'),
+var faunadb = require('faunadb'),
     q = faunadb.query;
 
 var adminClient = new faunadb.Client({
     secret: process.env.FAUNADB_SERVER_SECRET
 });
 
-// function rebuildSite() {
-//   let url = `https://api.netlify.com/build_hooks/${process.env.build_hook_id}`;
-//   console.log(url);
-//   const options = {
-//     method: "POST",
-//     uri: url
-//   }
-//   return rp(options)
-//     .then(() => console.log("Posted and Rebuilding"))
-//     .catch((err) => console.log(err));
-// }
 
-async function createBookmark(value) {
-  return new Promise((resolve, reject) => {
-    let values = {
-      url: value.url,
-      pageTitle: value.pagetitle,
-      description: value.description
-    }
+const getDetails = async function(url) {
+  const data = rp(url).then(function(htmlString) {
+    const $ = cheerio.load(htmlString);
+    const title = $('head > title').text();
+    const description = $('meta[name="description"]').attr('content');
+    return {
+      pageTitle: title,
+      description: description
+    };
+  });
+  return data
+}
 
-    console.log('values', values);
-
-    return adminClient.query(q.Create(q.Collection("links"), values))
+const saveBookmark = async function(details) {
+  const data = {
+    data: details
+  };
+  return adminClient.query(q.Create(q.Collection("links"), data))
     .then((response) => {
       console.log("success", response)
       /* Success! return the response with statusCode 200 */
-      return callback(null, {
+      return {
         statusCode: 200,
         body: JSON.stringify(response)
-      })
+      }
     }).catch((error) => {
       console.log("error", error)
       /* Error! return the error with statusCode 400 */
-      return callback(null, {
+      return  {
         statusCode: 400,
         body: JSON.stringify(error)
-      })
+      }
     })
-
-  });
-    
-
 }
 
-
-function getDetails(html) {
-  console.log('getting details');
-  const details = {};
-  const $ = cheerio.load(html);
-  console.log($('title'));
-  details['pagetitle'] = $('head > title').text();
-  details['description'] = $('meta[name="description"]').attr('content');
-  return details
-}
-
-
-
-exports.handler = async function create(event, context) {
-    // try {
-    //   console.log('inside try in handler');
-    //   const details = await rp(event.queryStringParameters.url)
-    //   .then((html) => getDetails(html))
-    //   .catch((err) => console.log(err));
-      
-
-    //   const body = await createBookmark({url: event.queryStringParameters.url, ...details})
-    //   .then((data) => {
-    //     // rebuildSite();
-    //     return data
-    //   })
-    //   .catch((err) => console.log(err));
-      
-
-    //   return { statusCode: 200, body };
-    // } catch (err) {
-    //   return { statusCode: 500, body: "lambda failure code!" };
-    // }
-    return "inside create"
-  };
-  
+export { getDetails, saveBookmark }
