73eb89d142969b153f651b1f1f84e2a39dc4c9cb
Bryan Robinson <bryanlrobinson@gmail.com>
2019-09-16 14:25:09 -0500

    Reworked POST request

--- lambda/bookmarks.js
+++ lambda/bookmarks.js
@@ -16,16 +16,19 @@ exports.handler = async function(event, context) {
 
     if (savedResponse.statusCode === 200) {
 
-      if (process.env.CONTEXT === 'production') {
         var options = {
           method: 'POST',
           uri: 'https://api.netlify.com/build_hooks/5d7fa6175504dfd43377688c',
-          body: {}      
+          body: {},
+          json: true      
         };
       
         console.log('Rebuilding the site ... ');
-        rp(options);
-      }
+        const returned = await rp(options).then(function(res) {
+          console.log('Successfully hit webhook', res);
+        }).catch(function(err) {
+          console.log('Error:', err);
+        });
 
       return { statusCode: 200, body: savedResponse.body }
     } else {
