5cf8c0b9b2b086838233548229b718854b068020
Bryan Robinson <bryanlrobinson@gmail.com>
2019-11-01 11:27:29 -0500

    Adds dropdown search for bookmarks in JS and styles

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,7 @@
 module.exports = function(config) {
 
     config.addPassthroughCopy('src/styles.css');
+    config.addPassthroughCopy('src/js/script.js');
     return {
         passthroughFileCopy: true,
         dir: {
